8c78b85a5e3a434858420bc0f79b2eb05575bf0a
Modman <aleksey.modman@yandex.ru>
2024-08-03 01:41:13 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+const image = document.getElementById('preloader');
+image.width = window.innerWidth;
+image.height = window.innerHeight;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
