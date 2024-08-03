8925309dd3f5c02768c4b685d8f58b9b6c081816
Modman <aleksey.modman@yandex.ru>
2024-08-03 22:15:49 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -2,25 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import mysql from 'mysql';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const conn = mysql.createConnection({
-    host: 'mysql.b559fbfa7208.hosting.myjino.ru',
-    port: 3306,
-    user: 'j03809714_modman',
-    password: "aLeksey2011!",
-    database: 'j03809714_modmancomm'
-});
 
-conn.connect(function (err) {
-    if (err) {
-        return "Ошибка соединения с базой данных";
-    }
-    else {
-        console.log("Connection successful");
-    }
-})
 root.render(
   <React.StrictMode>
     <App />
