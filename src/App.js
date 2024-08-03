6028b0ae83d12e859072b703b4cbec2dfa390ac5
Modman <aleksey.modman@yandex.ru>
2024-08-03 23:06:31 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 
-
+const tg = window.Telegram.WebApp;
 
 function App() {
+  useEffect(() =>{
+    tg.ready();
+  }, []);
   const [loading, setLoading] = useState(true);
   const spinner = document.getElementById('spinner');
   if(spinner){
