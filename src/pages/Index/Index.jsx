5d2dd4844afa85236358c758212a1e20e4fe9b50
Modman <aleksey.modman@yandex.ru>
2024-08-04 20:06:05 +0300

    Update

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import {useTelegram} from "../../components/hooks/useTelegram";
+import {tg} from "../../components/hooks/useTelegram";
 
 const Index = () => {
-    const {tg, user, username, avatar} = useTelegram();
 
     return (
         <div>
-            <img src={avatar} alt=""/>
-            <h1>{username}</h1>
+            <img src={tg.initDataUnsafe?.user?.photo_url} alt=""/>
+            <h1>{tg.initDataUnsafe?.user?.username}</h1>
         </div>
     );
 };
