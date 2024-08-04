a84cf0cc34a56e1ee0bab9cd1a0a87ea7e209968
Modman <aleksey.modman@yandex.ru>
2024-08-05 02:22:21 +0300

    Update

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -7,11 +7,11 @@ const Bottom = () => {
             <input type="radio" id="one" name="buttons" checked/>
             <label htmlFor="one" className="icons home"><span className="glyphicon glyphicon-home"></span></label>
             <input type="radio" id="two" name="buttons"/>
-            <label htmlFor="two" className="icons search"><span className="glyphicon glyphicon-search"></span></label>
+            <label htmlFor="two" className="icons search"><span className="glyphicon glyphicon-list-alt"></span></label>
             <input type="radio" id="three" name="buttons" />
-            <label htmlFor="three" className="icons heart"><span className="glyphicon glyphicon-heart"></span></label>
+            <label htmlFor="three" className="icons heart"><span className="glyphicon glyphicon-fire"></span></label>
             <input type="radio" id="four" name="buttons" disabled={true}/>
-            <label htmlFor="four" className="icons bell"><span className="glyphicon glyphicon-wallet"></span></label>
+            <label htmlFor="four" className="icons bell"><span className="glyphicon glyphicon-btc"></span></label>
             <div id="box">
             </div>
             <div id="body"></div>
