6ebd3febd2c32e282425f124011d8934d7768021
Modman <aleksey.modman@yandex.ru>
2024-08-04 20:14:48 +0300

    Update

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -6,7 +6,6 @@ const Index = () => {
     const {tg, user} = useTelegram();
     return (
         <div>
-            <img src={user?.photo_url} alt=""/>
             <h1>{user?.username}</h1>
         </div>
     );
