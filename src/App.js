fe3a2e482ebcdbd8864b49d875021884c8ebba94
Modman <aleksey.modman@yandex.ru>
2024-08-04 17:35:21 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
 
   const [loading, setLoading] = useState(true);
   const spinner = document.getElementById('spinner');
-  if(spinner){
+  /*if(spinner){
     setTimeout(() => {
       spinner.style.display = 'none';
       setLoading(false);
     },8000)
-  }
+  }*/
   const wind = document.getElementById('windowsize');
   wind.style.height = window.innerHeight+'px';
   wind.style.overflow = 'hidden';
