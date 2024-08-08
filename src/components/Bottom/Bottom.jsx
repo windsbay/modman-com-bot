0a169e0899e7985531838b06333434dd3fbf89d2
Modman <aleksey.modman@yandex.ru>
2024-08-08 15:07:14 +0300

    Update

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -5,23 +5,19 @@ import {redirect} from "react-router-dom";
 const Bottom = () => {
     return (
         <div className="bottom">
-            <input type="radio" id="one" name="buttons" checked/>
-            <label htmlFor="one" className="icons home"><span className="glyphicon glyphicon-home"></span></label>
-            <input type="radio" id="two" name="buttons" />
-            <label htmlFor="two" className="icons search"><span className="glyphicon glyphicon-list-alt"></span></label>
-            <input type="radio" id="three" name="buttons" />
-            <label htmlFor="three" className="icons heart"><span className="glyphicon glyphicon-fire"></span></label>
-            <input type="radio" id="four" name="buttons" disabled={false}/>
-            <label htmlFor="four" className="icons bell"><span className="glyphicon glyphicon-btc"></span></label>
-            <div id="box">
-            </div>
-            <div id="body"></div>
-            <span className="title home">Home</span>
-            <span className="title search">Tasks</span>
-            <span className="title heart">Friends</span>
-            <span className="title bell">Wallet</span>
-            <div className="border"></div>
-            <div className="effect"></div>
+                <ul>
+                        <li>
+                                <a href="/">
+                                        <!-- this anchor text for link your home to another page -->
+                                        <div className="icon">
+                                                <i className="fa fa-home" aria-hidden="true"></i><!-- this is home icon link get form fornt-awesome icon for home button -->
+                                                <i className="fa fa-home" aria-hidden="true"></i><!-- copy and paste the home icon link here for hover effect -->
+                                        </div>
+                                        <div className="name"><span data-text="Home">Home</span></div>
+                                        <!-- we are create first menu item name home -->
+                                </a>
+                        </li>
+                </ul>
         </div>
     );
 };
