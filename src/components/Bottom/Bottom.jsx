eea454c6ffc303254642994457a8f8447a51ce50
Modman <aleksey.modman@yandex.ru>
2024-08-08 15:31:47 +0300

    Update

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -5,17 +5,44 @@ import {redirect} from "react-router-dom";
 const Bottom = () => {
     return (
         <div className="bottom">
-                <ul>
-                        <li>
-                                <a href="/">
-                                        <div className="icon">
-                                                <i className="fa fa-home" aria-hidden="true"></i>
-                                                <i className="fa fa-home" aria-hidden="true"></i>
-                                        </div>
-                                        <div className="name"><span data-text="Home">Home</span></div>
-                                </a>
-                        </li>
-                </ul>
+            <ul>
+                <li>
+                    <a href="/">
+                        <div className="icon">
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                        </div>
+                        <div className="name"><span data-text="Home">Home</span></div>
+                    </a>
+                </li>
+                <li>
+                    <a href="/tasks">
+                        <div className="icon">
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                        </div>
+                        <div className="name"><span data-text="Tasks">Tasks</span></div>
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        <div className="icon">
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                        </div>
+                        <div className="name"><span data-text="Friends">Friends</span></div>
+                    </a>
+                </li>
+                <li aria-disabled={true}>
+                    <a href="/wallet">
+                        <div className="icon">
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                            <i className="fa fa-home" aria-hidden="true"></i>
+                        </div>
+                        <div className="name"><span data-text="Home">Home</span></div>
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
