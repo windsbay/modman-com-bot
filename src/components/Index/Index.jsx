a82b169aba999fa0f6b8c89ea4718dfb304a767b
Modman <aleksey.modman@yandex.ru>
2024-08-08 23:14:08 +0300

    Update

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -13,13 +13,16 @@ const Index = () => {
 
     return (
         <div className={'index'}>
-            <h1>Home Page</h1>
-            <div>
-                <h1>{user?.username}</h1>
-            </div>
-            <div>
-                <h2>15,000</h2>
-            </div>
+            <canvas id="canvas">
+                <h1>Home Page</h1>
+                <div>
+                    <h1>{user?.username}</h1>
+                </div>
+                <div>
+                    <h2>15,000</h2>
+                </div>
+            </canvas>
+
         </div>
     );
 };
