424c1a44b5a7ad39834bb312c5b8f3456834e54d
Modman <aleksey.modman@yandex.ru>
2024-08-08 16:50:27 +0300

    Update

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -4,45 +4,12 @@ import {redirect} from "react-router-dom";
 
 const Bottom = () => {
     return (
-        <div className="bottom">
-            <ul>
-                <li>
-                    <a href="/">
-                        <div className="icon">
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                        </div>
-                        <div className="name"><span data-text="Home">Home</span></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="/tasks">
-                        <div className="icon">
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                        </div>
-                        <div className="name"><span data-text="Tasks">Tasks</span></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="/">
-                        <div className="icon">
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                        </div>
-                        <div className="name"><span data-text="Friends">Friends</span></div>
-                    </a>
-                </li>
-                <li aria-disabled={true}>
-                    <a href="/wallet">
-                        <div className="icon">
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                            <i className="fa fa-home" aria-hidden="true"></i>
-                        </div>
-                        <div className="name"><span data-text="Home">Home</span></div>
-                    </a>
-                </li>
-            </ul>
+        <div>
+            <nav>
+                <a className="active" href="/"><i className="fas fa-fw fa-home"></i> Home</a>
+                <a href="/tasks"><i className="fas fa-fw fa-code"></i> Tasks</a>
+                <a href="/friends"><i className="fas fa-fw fa-briefcase"></i> Friends</a>
+            </nav>
         </div>
     );
 };
