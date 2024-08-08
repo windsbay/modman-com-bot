3b04b6e81ace70051125accf603745c07d36ac8d
Modman <aleksey.modman@yandex.ru>
2024-08-08 15:11:23 +0300

    Update

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -8,13 +8,11 @@ const Bottom = () => {
                 <ul>
                         <li>
                                 <a href="/">
-                                        <!-- this anchor text for link your home to another page -->
                                         <div className="icon">
-                                                <i className="fa fa-home" aria-hidden="true"></i><!-- this is home icon link get form fornt-awesome icon for home button -->
-                                                <i className="fa fa-home" aria-hidden="true"></i><!-- copy and paste the home icon link here for hover effect -->
+                                                <i className="fa fa-home" aria-hidden="true"></i>
+                                                <i className="fa fa-home" aria-hidden="true"></i>
                                         </div>
                                         <div className="name"><span data-text="Home">Home</span></div>
-                                        <!-- we are create first menu item name home -->
                                 </a>
                         </li>
                 </ul>
