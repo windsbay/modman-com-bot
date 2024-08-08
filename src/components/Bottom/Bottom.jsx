47906f883be852478c802ad0cec3ab7657fd9b7a
Modman <aleksey.modman@yandex.ru>
2024-08-08 17:07:53 +0300

    Update

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -6,9 +6,24 @@ const Bottom = () => {
     return (
         <div>
             <nav>
-                <a className="active" href="/"><i className="fas fa-fw fa-home"></i> Home</a>
-                <a href="/tasks"><i className="fas fa-fw fa-code"></i> Tasks</a>
-                <a href="/friends"><i className="fas fa-fw fa-briefcase"></i> Friends</a>
+                <a className="active" href="/">
+                    <div className={'icon'}>
+                        <i className="fas fa-fw fa-home" aria-hidden={true}></i>
+                    </div>
+                    <div className={'name'}><span data-text={'Home'}>Home</span></div>
+                </a>
+                <a href={'/tasks'}>
+                    <div className={'icon'}>
+                        <i className="fas fa-fw fa-code" aria-hidden={true}></i>
+                    </div>
+                    <div className={'name'}><span data-text={'Tasks'}>Tasks</span></div>
+                </a>
+                <a href={'/friends'}>
+                    <div className={'icon'}>
+                        <i className="fas fa-fw fa-briefcase" aria-hidden={true}></i>
+                    </div>
+                    <div className={'name'}><span data-text={'Friends'}>Friends</span></div>
+                </a>
             </nav>
         </div>
     );
