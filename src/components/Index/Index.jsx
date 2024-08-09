761d9dc4053d6e31031119793dba8f4a47965d83
Modman <aleksey.modman@yandex.ru>
2024-08-09 06:50:45 +0300

    Update

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -8,17 +8,17 @@ import {useTelegram} from "../hooks/useTelegram";
 const Index = () => {
 
     const {user} = useTelegram();
-    const image = document.querySelector('.image');
+    let image = document.querySelector('.image');
 
-    image.addEventListener('click', () => {
+    const click = () => {
         image.classList.toggle('active');
-    });
+    }
 
 
     return (
         <div className={'index'}>
                 <h1>{user?.username}</h1>
-                <img src="/img/modmancoin.png" className={'image'} alt="" />
+                <img src="/img/modmancoin.png" className={'image'} onClick={click} />
                 <h2>15,000</h2>
         </div>
     );
