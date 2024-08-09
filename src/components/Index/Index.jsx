06c7fb075877f1af9a7ffe7676a07e33f8de51eb
Modman <aleksey.modman@yandex.ru>
2024-08-09 07:05:15 +0300

    Update

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Index.css';
 
 import {useTelegram} from "../hooks/useTelegram";
+import MainImage from "../UI/MainImage/MainImage";
 
 
 
@@ -18,7 +19,7 @@ const Index = () => {
     return (
         <div className={'index'}>
                 <h1>{user?.username}</h1>
-                <img src="/img/modmancoin.png" className={'image'} onClick={click} />
+                <MainImage />
                 <h2>15,000</h2>
         </div>
     );
---
+++ src/components/UI/MainImage/MainImage.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+    width: 300px;
+    height: 300px;
+    margin-top: 50px;
+    margin-bottom: 20px;
+    transition: transform 0.2s ease-in-out;
+
+  &.active {
+    transform: scale(1.5);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+  }
+`;
+
+const MainImage = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <Image
+            src="/img/modmancoin.png"
+            className={isActive? 'active' : ''}
+            onClick={handleClick}
+        />
+    );
+};
+
+export default MainImage;
