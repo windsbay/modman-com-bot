04e300dc0012982254d8fcf15aa900a697f3dd52
Modman <aleksey.modman@yandex.ru>
2024-08-10 22:35:52 +0300

    Update

--- src/Pages/Friends/Friends.jsx
+++ src/Pages/Friends/Friends.jsx
@@ -11,7 +11,7 @@ const Friends = () => {
             <h2>Invite Frens</h2>
             <div className={'refer-rewards'}>
                     <p><span className={'projectSym'}>M</span> 0</p>
-                    <Button style={'width: 100px; height: 50px'}>Claim</Button>
+                    <Button style={'width: 200px; height: 50px'}>Claim</Button>
             </div>
         </div>
     );
