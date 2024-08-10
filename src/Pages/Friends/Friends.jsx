1ae11d7917f60becdc8b18ac4698257a59675bcd
Modman <aleksey.modman@yandex.ru>
2024-08-10 19:42:54 +0300

    Update

--- src/Pages/Friends/Friends.jsx
+++ src/Pages/Friends/Friends.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Friends.css';
+import twemoji from 'twemoji';
 
 const Friends = () => {
     return (
         <div className={'friends'}>
-            <h1>:family_mwbb:</h1>
+            <h1>{twemoji.parse(':family_mwbb:')}</h1>
         </div>
     );
 };
