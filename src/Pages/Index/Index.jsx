6cb6dc65dbceb517c4e1bab0b35f09741d15dea1
Modman <aleksey.modman@yandex.ru>
2024-08-18 17:02:12 +0300

    Update

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -22,8 +22,7 @@ const Index = () => {
     return (
         <div className={'index'}>
             {ok ? <Button>Admin Panel</Button> : ""}
-                <h1>{user?.id}</h1>
-                <h1>{user?.username}</h1>
+                <h1>{user?.first_name}</h1>
                 <MainImage />
                 <h2><span className={'projectSym'}>M </span>15,000</h2>
         </div>
